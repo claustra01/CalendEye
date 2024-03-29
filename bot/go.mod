409b7073c193d25384b9887519d38423a3f42549
claustra01 <shinya430.g@gmail.com>
2024-03-29 16:14:10 +0900

    refactor: remove bot sdk and

--- bot/go.mod
+++ bot/go.mod
@@ -1,8 +1,5 @@
-module github.com/claustra01/CalendEye
+module github.com/claustra01/calendeye
 
 go 1.22.1
 
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/line/line-bot-sdk-go/v8 v8.4.0
-)
+require github.com/joho/godotenv v1.5.1
--- bot/server.go
+++ bot/server.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/claustra01/calendeye/linebot"
+	"github.com/claustra01/calendeye/webhook"
 	"github.com/joho/godotenv"
-	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
-	"github.com/line/line-bot-sdk-go/v8/linebot/webhook"
 )
 
 func main() {
@@ -26,7 +25,7 @@ func main() {
 	channelToken := os.Getenv("LINE_CHANNEL_TOKEN")
 
 	// Initialize LINE bot
-	bot, err := messaging_api.NewMessagingApiAPI(channelToken)
+	bot, err := linebot.NewBot(channelToken)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,9 +38,9 @@ func main() {
 		if err != nil {
 			log.Printf("Cannot parse request: %+v\n", err)
 			if errors.Is(err, webhook.ErrInvalidSignature) {
-				w.WriteHeader(400)
+				w.WriteHeader(http.StatusBadRequest)
 			} else {
-				w.WriteHeader(500)
+				w.WriteHeader(http.StatusInternalServerError)
 			}
 			return
 		}
@@ -55,34 +54,16 @@ func main() {
 				switch message := e.Message.(type) {
 				case webhook.TextMessageContent:
 					if _, err = bot.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
+						&linebot.ReplyMessageRequest{
 							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: message.Text,
-								},
+							Messages: []linebot.MessageInterface{
+								linebot.NewTextMessage(message.Text),
 							},
 						},
 					); err != nil {
 						log.Print(err)
 					} else {
 						log.Println("Sent text reply.")
-					}
-				case webhook.StickerMessageContent:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerId, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
-							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: replyMessage,
-								},
-							},
-						}); err != nil {
-						log.Print(err)
-					} else {
-						log.Println("Sent sticker reply.")
 					}
 				default:
 					log.Printf("Unsupported message content: %T\n", e.Message)
@@ -99,7 +80,7 @@ func main() {
 	if port == "" {
 		port = "5000"
 	}
-	fmt.Println("http://localhost:" + port + "/")
+	log.Println("Server started!")
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
