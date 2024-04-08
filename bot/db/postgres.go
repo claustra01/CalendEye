5e114d271e9749c40a885795a9f11be0b4a15015
claustra01 <shinya430.g@gmail.com>
2024-04-08 14:00:04 +0900

    fix: postgres connection

--- bot/db/postgres.go
+++ bot/db/postgres.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -9,12 +10,21 @@ import (
 )
 
 func (db *SqlHandler) Connect() {
-	conn, err := sql.Open("postgres", os.Getenv("POSTGRES_DATABASE_URL"))
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+
+	conn, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Connected to database!")
 	db.Conn = conn
+
+	err = db.Conn.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Connected to database!")
 }
 
 func (db *SqlHandler) Close() {
