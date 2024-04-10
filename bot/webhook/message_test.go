8ce04d29466ec37f3c7bcdc8e95a2ada4577ea7c
claustra01 <shinya430.g@gmail.com>
2024-04-10 13:05:51 +0900

    add: image message unmarshal test

--- bot/webhook/message_test.go
+++ bot/webhook/message_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestMessageEvent_UnmarshalJSON(t *testing.T) {
 	tests := []struct {
-		name     string
-		data     []byte
-		expected MessageEvent
-		wantErr  bool
+		name    string
+		data    []byte
+		expect  MessageEvent
+		wantErr bool
 	}{
 		{
 			name: "Valid text message",
-			data: []byte(`{"type":"message","mode":"test_mode","timestamp":123456789,"source":{"type":"user","userId":"123"},"webhookEventId":"event_id","deliveryContext":{"isRedelivery":true},"message":{"type":"text","id":"message_id"},"replyToken":"reply_token"}`),
-			expected: MessageEvent{
+			data: []byte(`{"type":"message","mode":"test_mode","timestamp":123456789,"source":{"type":"user","userId":"123"},"webhookEventId":"event_id","deliveryContext":{"isRedelivery":true},"message":{"type":"text","id":"message_id","text":"Hello, world!"},"replyToken":"reply_token"}`),
+			expect: MessageEvent{
 				Event: Event{
 					Type:            "message",
 					Mode:            "test_mode",
@@ -27,15 +27,58 @@ func TestMessageEvent_UnmarshalJSON(t *testing.T) {
 					DeliveryContext: DeliveryContext{IsRedelivery: true},
 				},
 				ReplyToken: "reply_token",
-				Message:    TextMessageContent{MessageContent: MessageContent{Type: "text", Id: "message_id"}},
+				Message: TextMessageContent{
+					MessageContent: MessageContent{Type: "text", Id: "message_id"},
+					Text:           "Hello, world!",
+				},
 			},
 			wantErr: false,
 		},
 		{
-			name:     "Invalid JSON",
-			data:     []byte(`invalid json`),
-			expected: MessageEvent{},
-			wantErr:  true,
+			name: "Valid line image message",
+			data: []byte(`{"type":"message","mode":"test_mode","timestamp":123456789,"source":{"type":"user","userId":"123"},"webhookEventId":"event_id","deliveryContext":{"isRedelivery":true},"message":{"type":"image","id":"message_id","contentProvider":{"type":"line"}},"replyToken":"reply_token"}`),
+			expect: MessageEvent{
+				Event: Event{
+					Type:            "message",
+					Mode:            "test_mode",
+					Timestamp:       123456789,
+					Source:          UserSource{Source: Source{Type: "user"}, UserId: "123"},
+					WebhookEventId:  "event_id",
+					DeliveryContext: DeliveryContext{IsRedelivery: true},
+				},
+				ReplyToken: "reply_token",
+				Message: ImageMessageContent{
+					MessageContent:  MessageContent{Type: "image", Id: "message_id"},
+					ContentProvider: ImageContentProvider{Type: "line", OriginalContentUrl: "", PreviewImageUrl: ""},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Valid external image message",
+			data: []byte(`{"type":"message","mode":"test_mode","timestamp":123456789,"source":{"type":"user","userId":"123"},"webhookEventId":"event_id","deliveryContext":{"isRedelivery":true},"message":{"type":"image","id":"message_id","contentProvider":{"type":"external","originalContentUrl":"original_url","previewImageUrl":"preview_url"}},"replyToken":"reply_token"}`),
+			expect: MessageEvent{
+				Event: Event{
+					Type:            "message",
+					Mode:            "test_mode",
+					Timestamp:       123456789,
+					Source:          UserSource{Source: Source{Type: "user"}, UserId: "123"},
+					WebhookEventId:  "event_id",
+					DeliveryContext: DeliveryContext{IsRedelivery: true},
+				},
+				ReplyToken: "reply_token",
+				Message: ImageMessageContent{
+					MessageContent:  MessageContent{Type: "image", Id: "message_id"},
+					ContentProvider: ImageContentProvider{Type: "external", OriginalContentUrl: "original_url", PreviewImageUrl: "preview_url"},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:    "Invalid JSON",
+			data:    []byte(`invalid json`),
+			expect:  MessageEvent{},
+			wantErr: true,
 		},
 	}
 
@@ -49,8 +92,8 @@ func TestMessageEvent_UnmarshalJSON(t *testing.T) {
 				return
 			}
 
-			if !reflect.DeepEqual(cr, tt.expected) {
-				t.Errorf("MessageEvent.UnmarshalJSON() got = %v, want %v", cr, tt.expected)
+			if !reflect.DeepEqual(cr, tt.expect) {
+				t.Errorf("MessageEvent.UnmarshalJSON() got = %v, want %v", cr, tt.expect)
 			}
 		})
 	}
