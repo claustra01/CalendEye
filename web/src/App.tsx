88779cbd9948b966532ea853aebe61c32d557af9
claustra01 <shinya430.g@gmail.com>
2024-04-10 15:19:52 +0900

    Fix: lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import OAuth2 from "./pages/oauth2";
+import Home from './pages/home';
+import OAuth2 from './pages/oauth2';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
 				<Route path="/oauth2" element={<OAuth2 />} />
-      </Routes>
-    </BrowserRouter>
-  );
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- web/src/pages/oauth2.tsx
+++ web/src/pages/oauth2.tsx
@@ -1,9 +1,9 @@
 const OAuth2 = () => {
-  return (
-    <div>
-      <h1>OAuth2 Page Template</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>OAuth2 Page Template</h1>
+		</div>
+	);
+};
 
-export default OAuth2
+export default OAuth2;
