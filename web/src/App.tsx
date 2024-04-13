d98230846f8c6ebb48bf659e915297f26f3441b2
claustra01 <shinya430.g@gmail.com>
2024-04-13 17:37:23 +0900

    add: nouser page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/oauth2" element={<OAuth2 />} />
+				<Route path="/nouser" element={<div>LINE友達登録後、再度お試しください。</div>} />
 			</Routes>
 		</BrowserRouter>
 	);
