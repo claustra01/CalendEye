033fd43ffd501c24208954bbe22163ea7f2c67a2
claustra01 <shinya430.g@gmail.com>
2024-04-13 17:37:37 +0900

    add: user exist check before oauth2

--- web/src/pages/oauth2.tsx
+++ web/src/pages/oauth2.tsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const OAuth2 = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const apiUrl = import.meta.env.VITE_API_URL
+
+	const query = new URLSearchParams(location.search);
+	const lineId = query.get("id");
+
+	const fetchData = async () => {
+		try {
+			console.log(`${apiUrl}/users?id=${lineId}`)
+			const response = await fetch(`${apiUrl}/user?id=${lineId}`);
+			const data = await response.json();	
+			if (!response.ok) {
+				console.error("Failed to fetch user data: ", data);
+				navigate("/nouser");
+			}
+		} catch(error) {
+			console.error("Failed to fetch user data: ", error);
+			navigate("/nouser");
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
-		<div>
-			<h1>OAuth2 Page Template</h1>
-		</div>
+		<></>
 	);
 };
 
