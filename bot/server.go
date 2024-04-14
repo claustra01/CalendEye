88965432981caeb027ee6cb497fa0e9f2f669d2f
claustra01 <shinya430.g@gmail.com>
2024-04-14 16:59:59 +0900

    fix: cors middleware

--- bot/server.go
+++ bot/server.go
@@ -15,8 +15,14 @@ import (
 func CORSHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, PATCH")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
