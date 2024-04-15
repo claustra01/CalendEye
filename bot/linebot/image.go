3fb4c2f96896135f800c09a6dec865f1f953d73a
claustra01 <shinya430.g@gmail.com>
2024-04-15 14:02:42 +0900

    add: fetch external image function

--- bot/linebot/image.go
+++ bot/linebot/image.go
@@ -27,3 +27,23 @@ func (c *LineBot) FetchLineImage(ctx context.Context, id string) (image.Image, s
 	img, format, err := image.Decode(resp.Body)
 	return img, format, nil
 }
+
+func (c *LineBot) FetchExternalImage(ctx context.Context, url string) (image.Image, string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, "", err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if resp.StatusCode != http.StatusOK {
+		return nil, "", fmt.Errorf("failed to fetch image: %s", resp.Status)
+	}
+	if err != nil {
+		return nil, "", err
+	}
+	defer resp.Body.Close()
+
+	img, format, err := image.Decode(resp.Body)
+	return img, format, nil
+}
