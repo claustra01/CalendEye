46fe6a0e5e644d3ecb266853e29884ce8a87348f
claustra01 <shinya430.g@gmail.com>
2024-04-15 14:02:28 +0900

    add: fetch image function

---
+++ bot/linebot/image.go
@@ -0,0 +1,29 @@
+package linebot
+
+import (
+	"context"
+	"fmt"
+	"image"
+	"net/http"
+)
+
+func (c *LineBot) FetchLineImage(ctx context.Context, id string) (image.Image, string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://api-data.line.me/v2/bot/message/"+id+"/content", nil)
+	if err != nil {
+		return nil, "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.channelToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if resp.StatusCode != http.StatusOK {
+		return nil, "", fmt.Errorf("failed to fetch image: %s", resp.Status)
+	}
+	if err != nil {
+		return nil, "", err
+	}
+	defer resp.Body.Close()
+
+	img, format, err := image.Decode(resp.Body)
+	return img, format, nil
+}
